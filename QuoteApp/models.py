1e394f9cdc95c62af293eb67c85efead172a4514
hb1113 <hbarami@gmail.com>
2021-04-07 19:40:02 +0100

    create QuotePost model

--- QuoteApp/models.py
+++ QuoteApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+class QuotePost(models.Model):
+
+    text = models.CharField(max_length=256)
+    author = moels.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    shares = models.IntegerField(default=0)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f"{ self.text }, by: {self.author}"
