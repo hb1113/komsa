5cfc5ae95a4cf26573474bb3cce7934d4d22430d
hb1113 <hbarami@gmail.com>
2021-04-09 08:25:37 +0100

    create Comment model

--- QuoteApp/models.py
+++ QuoteApp/models.py
@@ -19,3 +19,22 @@ class QuotePost(models.Model):
 
     def __str__(self):
         return f"{ self.text }, by: {self.author}"
+
+
+class Comment(models.Model):
+
+        text = models.CharField(max_length=256)
+        post = models.ForeignKey(post, on_delete=models.CASCADE, related_name='comment')
+        parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+        author = models.ForeignKey(User, on_delete=models.CASCADE)
+        created_date = models.DateTimeField(default=timezone.now)
+        published_date = models.DateTimeField(blank=True, null=True)
+        likes = models.IntegerField(default=0)
+        comments = models.IntegerField(default=0)
+
+        def publish(self):
+            self.published_date = timezone.now()
+            self.save()
+
+        def __str__(self):
+            return f"{ self.text }, by: {self.author}"
