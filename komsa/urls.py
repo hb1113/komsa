0ba874369f09373eac6f6254e0299e764865a68e
hb1113 <hbarami@gmail.com>
2021-04-09 21:51:26 +0100

    create MembersApp and register form

--- komsa/urls.py
+++ komsa/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quotes/', include('QuoteApp.urls'))
+    path('', include('QuoteApp.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('MembersApp.urls')),
 ]
