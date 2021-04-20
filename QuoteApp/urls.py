13d90031ecc448d0af20f2faeaa0d2ae63df4fa4
hb1113 <hbarami@gmail.com>
2021-04-20 14:27:52 +0100

    create urls for views in QuoteApp

--- QuoteApp/urls.py
+++ QuoteApp/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 app_name = 'quote'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.QuoteListView.as_view(), name='list'),
+    path('<int:id>', views.QuoteDetailView.as_view(), name='detail'),
+    path('newquote/', views.CreateQuotePostView.as_view(), name='new'),
+    path('update/<int:id>/', views.UpdateQuotePostView.as_view(), name='update'),
+    path('delete/<int:id>/', views.DeleteQuotePostView.as_view(), name='delete'),
+    path('drafts/', views.DraftQuoteListView.as_view(), name='draft'),
+    path('about/', views.AboutQuoteView.as_view(), name='about'),
 ]
