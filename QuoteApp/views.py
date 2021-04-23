614c7647d16040b1dde2c1ec9697226c42796079
hb1113 <hbarami@gmail.com>
2021-04-23 11:36:35 +0100

    modify and add some views for quote and comment

--- QuoteApp/views.py
+++ QuoteApp/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView,DeleteView
 from .models import QuotePost, QuoteComment
 from .forms import QuoteForm, QuoteCommentForm
 from django.utils import timezone
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -22,6 +23,7 @@ class QuoteListView(ListView):
 
 class QuoteDetailView(DetailView):
     model = QuotePost
+    template_name = 'quoteapp/quote_detail.html'
 
 
 class CreateQuotePostView(CreateView):
@@ -29,6 +31,11 @@ class CreateQuotePostView(CreateView):
     form_class = QuoteForm
     model = QuotePost
     fields = ['text']
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class UpdateQuotePostView(UpdateView):
@@ -51,12 +58,44 @@ class DraftQuoteListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return QuotePost.objects.filter(published_date__isnull=True).order_by('created_date')
 
-
-class QuoteCommentsView(ListView):
-    model = QuoteComment
-    template_name = 'quoteapp/quote-comments.html'
-
-    def get_queryset(self):
-        return QuoteComment.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+###############################
 
 
+@login_required()
+def quote_comment_view(request, pk):
+    quote = get_object_or_404(QuotePost, pk=pk)
+    if request.method == 'POST':
+        form = QuoteCommentForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.quote = quote
+            obj.save()
+            return redirect('quote:detail', pk=quote.pk)
+    else:
+        form = QuoteCommentForm()
+
+    return render(request, 'quoteapp/comment_form.html', {'form': form})
+
+
+@login_required()
+def quote_comment_approve(request, pk):
+    comment = get_object_or_404(QuoteComment, pk=pk)
+    comment.approve()
+
+    return redirect('quoteapp:detail', pk=comment.quote.pk)
+
+
+@login_required()
+def quote_comment_delete(request, pk):
+    comment = get_object_or_404(QuoteComment, pk=pk)
+    quote_pk = QuoteComment.quote.pk
+    comment.delete()
+
+    return redirect('quoteapp:detail', pk=quote_pk)
+
+
+@login_required()
+def quote_post_publish(request, pk):
+    quote = get_object_or_404(QuotePost, pk=pk)
+    quote.publish()
+    return redirect('quoteapp:detail', pk=pk)
