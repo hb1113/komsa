6af8b535d45a10e07250721cf753a4f3caf19d20
hb1113 <hbarami@gmail.com>
2021-04-26 13:50:19 +0100

    farsi label and widgets form-control

--- MembersApp/forms.py
+++ MembersApp/forms.py
@@ -2,11 +2,33 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
 
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "name", "email", "password1", "password2"]
-        
+        fields = ["first_name", "email", "username", "password1", "password2"]
+
+        widgets = {
+            "username": forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'username',
+            }),
+            "first_name": forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'name',
+            }),
+            "password1": forms.PasswordInput(attrs={
+                'class': 'form-control',
+            }),
+            "password2": forms.PasswordInput(attrs={
+                'class': 'form-control',
+            }),
+
+        }
+        labels = {
+            'username': 'یوزر',
+            'first_name': 'نام',
+            'email': 'ایمیل',
+            'password1': 'پسورد',
+            'password2': 'پسورد',
+        }
