a7a324a36b238878a81c57a6575e02c07439017d
hb1113 <hbarami@gmail.com>
2021-04-26 13:28:17 +0100

    config media and static

--- komsa/settings.py
+++ komsa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'QuoteApp',
     'MembersApp',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -120,5 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
